4d4c76633924eeae4ead79e01237f9929573ea11
gamsaon <128341069+gamsaon@users.noreply.github.com>
2023-06-02 14:26:52 +0900

    Update check_camsize.py

--- python_ws/check_camsize.py
+++ python_ws/check_camsize.py
@@ -1,1 +1,11 @@
+import cv2
 
+capture = cv2.VideoCapture(0)
+wth = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+hit = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+print(wth)
+print(hit)
+
+#client cam size (1280,720)
+#server cam size (,)
